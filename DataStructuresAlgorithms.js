fde443dbbcc60a41448f4d095bc8bb979260ab0d
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-22 10:58:17 -0600

    revise comments

---
+++ DataStructuresAlgorithms.js
@@ -0,0 +1,46 @@
+//-------------ON LEET-----------------//
+
+  // String Operations
+
+  // Array
+
+  // Linked Lists
+    // Singly Linked
+    // Doubly Linked
+    // Circular Linked
+
+  // Queue & Stack
+
+  // Trees
+    // Binary Trees
+    // Binary Search Trees
+    // Tries
+    // Self Balancing Trees
+
+  // Traversing Trees
+    // Breadth First Search - BFS
+    // Depth First Search - DFS
+    // Preorder, Inorder, Postorder
+
+  // Hash Maps
+    // Handling Collisions
+
+
+//-------------NOT ON LEET-----------------//
+
+  // Data types
+
+  // Basic Bitwise Operations
+
+  // Heaps
+
+  // Graphs
+    // Dijkstra's Algorithm / A* Search
+
+  // Sorting algorithms
+    // Insertion
+      // Selection
+      // Merge
+      // Quick
+
+  // Time Complexities
