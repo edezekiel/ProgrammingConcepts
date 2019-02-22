226d3eb97e3ae8fac27d5740494a529c80eeff6e
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-22 14:23:09 -0600

    add to the outline

---
+++ ArrayString.js
@@ -0,0 +1,9 @@
+//----------------ARRAY INTRO----------------//
+
+
+//----------------Pivot Index----------------//
+
+  // return index where the sum of the numbers to the left of the index 
+  // is equal to the sum of the numbers to the right
+  // if none exists, return -1
+  // if multiple, return left-most pivot index
